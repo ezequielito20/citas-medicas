62fe8f2d5fced644cb4660cf6da1331f7725f7bd
Ezequiel Fischer <ezequielfischer1999@gmail.com>
2024-11-09 13:23:38 -0400

    se instalo la plantilla admin-let

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// rutas para el admin
+Route::get('/admin', [AdminController::class, 'index'])
+    ->name('admin.index')
+    ->middleware('auth')
+    ;
+
+
+
+Auth::routes();
+
