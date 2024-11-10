031c42ec9754a840cd5ded0d285fddc4ffd6a9d1
Ezequiel Fischer <ezequielfischer1999@gmail.com>
2024-11-10 10:55:15 -0400

    10 Listado de usuarios del controllador a la vista en el SISTEMA

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,5 +5,19 @@
 <div class="row">
     <h1>Listado de Usuarios</h1>
 </div>
+<hr>
+<div class="row">
+    @foreach ($users as $user)
+        <div class="col-md-4 ">
+            <div class="card">
+                <div class="card-body" style="background-color: #aafff4;">
+                    <h5 class="card-title">{{ $user->name }}</h5>
+                    <p class="card-text">{{ $user->email }}</p>
+                </div>
+            </div>
+        </div>
+        
+    @endforeach
+</div>
 
 @endsection()
