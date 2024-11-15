45df9b7004b5a167c7ed9c47fd8ebf3ef12c41dd
Ezequiel Fischer <ezequielfischer1999@gmail.com>
2024-11-15 15:34:18 -0400

    36 Migrate Pacientes

--- database/migrations/2024_11_15_150217_create_patients_table.php
+++ database/migrations/2024_11_15_150217_create_patients_table.php
@@ -14,10 +14,19 @@ return new class extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
 
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('address');
+            $table->string('names', length: 100);
+            $table->string('last_names', length: 100);
+            $table->string('ci', length: 9)->unique();
+            $table->string('email', length: 100)->unique();
+            $table->string('phone', length: 20)->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('gender', length: 1)->nullable();
+            $table->string('blood_group', length: 20)->nullable();
+            $table->string('allergies', length: 190)->nullable();
+            $table->string('emergency_contact', length: 100)->nullable();
+            $table->string('health_insurance_number', length: 100)->nullable();
+            $table->string('observations', length: 190)->nullable();
+            $table->string('address', length: 190)->nullable();
             
 
             $table->timestamps();
