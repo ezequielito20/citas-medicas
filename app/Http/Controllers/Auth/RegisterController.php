4fa30cbeb4db43bfe02a81b2d849c2a0f18dc191
Ezequiel Fischer <ezequielfischer1999@gmail.com>
2024-12-02 10:57:10 -0400

    82 Cargar Horario en la página principal en Sistema

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/login';
+    protected $redirectTo = '/index';
 
     /**
      * Create a new controller instance.
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
+        ])->assignRole('user');
         
     }
 }
